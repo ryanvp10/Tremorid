f83a25cd3af25dffe19fb251e3ef24f216a73645
ryanvp10 <ryansapta10@gmail.com>
2026-06-02 03:01:52 +0000

    chore: switch App to use Map2D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Map3D from './components/Map3D'
+import Map2D from './components/Map2D'
 import QuakeList from './components/QuakeList'
 import Timeline from './components/Timeline'
 
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <main className="flex flex-1 overflow-hidden">
         <div className="flex-1 relative">
-          <Map3D />
+          <Map2D />
         </div>
         <aside className="w-[360px] bg-bg-secondary border-l border-border overflow-y-auto">
           <QuakeList />
